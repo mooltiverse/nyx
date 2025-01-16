f7136a89f790a2c9fc73b3ed7a3b997a5fd4106f
Federico Lelli <federico.lelli@gmail.com>
2025-01-16 14:01:01 +0000

    test: add tests for configuration presets

--- src/go/nyx/configuration/presets_presets_test.go
+++ src/go/nyx/configuration/presets_presets_test.go
@@ -25,6 +25,9 @@ import (
 	"testing" // https://pkg.go.dev/testing
 
 	assert "github.com/stretchr/testify/assert" // https://pkg.go.dev/github.com/stretchr/testify/assert
+
+	ent "github.com/mooltiverse/nyx/src/go/nyx/entities"
+	utl "github.com/mooltiverse/nyx/src/go/utils"
 )
 
 func TestPresetsPresetByName(t *testing.T) {
@@ -43,4 +46,180 @@ func TestPresetsPresetByName(t *testing.T) {
 	simpleConfigurationLayer, err = PresetByName(EXTENDED_NAME)
 	assert.NoError(t, err)
 	assert.NotNil(t, simpleConfigurationLayer)
+}
+
+func TestPresetsLoadSimplePresetDirectly(t *testing.T) {
+	simpleConfigurationLayer, err := PresetByName(SIMPLE_NAME)
+	assert.NoError(t, err)
+	assert.NotNil(t, simpleConfigurationLayer)
+
+	changelog, err := simpleConfigurationLayer.GetChangelog()
+	assert.NoError(t, err)
+	assert.NotNil(t, changelog)
+
+	assert.Equal(t, 0, len(*changelog.GetSections()))
+	assert.Equal(t, 0, len(*changelog.GetSubstitutions()))
+
+	commitMessageConventions, err := simpleConfigurationLayer.GetCommitMessageConventions()
+	assert.NoError(t, err)
+	assert.NotNil(t, commitMessageConventions)
+
+	assert.Equal(t, 1, len(*commitMessageConventions.GetEnabled()))
+	assert.Equal(t, 1, len(*commitMessageConventions.GetItems()))
+
+	releaseTypes, err := simpleConfigurationLayer.GetReleaseTypes()
+	assert.NoError(t, err)
+	assert.NotNil(t, releaseTypes)
+
+	assert.Equal(t, 2, len(*releaseTypes.GetEnabled()))
+	assert.Equal(t, 2, len(*releaseTypes.GetItems()))
+
+	services, err := simpleConfigurationLayer.GetServices()
+	assert.NoError(t, err)
+	assert.NotNil(t, services)
+
+	assert.Equal(t, 0, len(*services))
+
+	substitutions, err := simpleConfigurationLayer.GetSubstitutions()
+	assert.NoError(t, err)
+	assert.NotNil(t, substitutions)
+
+	assert.Nil(t, substitutions.GetEnabled())
+	assert.Nil(t, substitutions.GetItems())
+}
+
+func TestPresetsLoadSimplePresetFromGlobalConfiguration(t *testing.T) {
+	configuration, _ := NewConfiguration()
+	simpleConfigurationLayer := NewSimpleConfigurationLayer()
+	simpleConfigurationLayer.SetPreset(utl.PointerToString(SIMPLE_NAME))
+	var scl ConfigurationLayer = simpleConfigurationLayer
+	configuration.WithPluginConfiguration(&scl)
+
+	changelog, err := configuration.GetChangelog()
+	assert.NoError(t, err)
+	assert.NotNil(t, changelog)
+
+	assert.Equal(t, 0, len(*changelog.GetSections()))
+	assert.Equal(t, 0, len(*changelog.GetSubstitutions()))
+
+	commitMessageConventions, err := configuration.GetCommitMessageConventions()
+	assert.NoError(t, err)
+	assert.NotNil(t, commitMessageConventions)
+
+	assert.Equal(t, 1, len(*commitMessageConventions.GetEnabled()))
+	assert.Equal(t, 1, len(*commitMessageConventions.GetItems()))
+
+	releaseTypes, err := configuration.GetReleaseTypes()
+	assert.NoError(t, err)
+	assert.NotNil(t, releaseTypes)
+
+	assert.Equal(t, 2, len(*releaseTypes.GetEnabled()))
+	assert.Equal(t, 2, len(*releaseTypes.GetItems()))
+
+	services, err := configuration.GetServices()
+	assert.NoError(t, err)
+	assert.NotNil(t, services)
+
+	assert.Equal(t, 0, len(*services))
+
+	substitutions, err := configuration.GetSubstitutions()
+	assert.NoError(t, err)
+	assert.NotNil(t, substitutions)
+
+	assert.Equal(t, 0, len(*substitutions.GetEnabled()))
+	assert.Equal(t, 0, len(*substitutions.GetItems()))
+}
+
+func TestPresetsLoadExtendedPresetDirectly(t *testing.T) {
+	simpleConfigurationLayer, err := PresetByName(EXTENDED_NAME)
+	assert.NoError(t, err)
+	assert.NotNil(t, simpleConfigurationLayer)
+
+	changelog, err := simpleConfigurationLayer.GetChangelog()
+	assert.NoError(t, err)
+	assert.NotNil(t, changelog)
+
+	assert.Equal(t, 4, len(*changelog.GetSections()))
+	assert.Equal(t, 0, len(*changelog.GetSubstitutions()))
+
+	commitMessageConventions, err := simpleConfigurationLayer.GetCommitMessageConventions()
+	assert.NoError(t, err)
+	assert.NotNil(t, commitMessageConventions)
+
+	assert.Equal(t, 2, len(*commitMessageConventions.GetEnabled()))
+	assert.Equal(t, 3, len(*commitMessageConventions.GetItems()))
+
+	releaseTypes, err := simpleConfigurationLayer.GetReleaseTypes()
+	assert.NoError(t, err)
+	assert.NotNil(t, releaseTypes)
+
+	assert.Equal(t, 9, len(*releaseTypes.GetEnabled()))
+	assert.Equal(t, 9, len(*releaseTypes.GetItems()))
+
+	services, err := simpleConfigurationLayer.GetServices()
+	assert.NoError(t, err)
+	assert.NotNil(t, services)
+
+	assert.Equal(t, 2, len(*services))
+	assert.Equal(t, ent.GITHUB, *(*services)["github"].GetType())
+	assert.Equal(t, 1, len(*(*services)["github"].GetOptions()))
+	assert.Equal(t, "{{#environmentVariable}}GITHUB_TOKEN{{/environmentVariable}}", (*(*services)["github"].GetOptions())["AUTHENTICATION_TOKEN"])
+	assert.Equal(t, ent.GITLAB, *(*services)["gitlab"].GetType())
+	assert.Equal(t, 1, len(*(*services)["gitlab"].GetOptions()))
+	assert.Equal(t, "{{#environmentVariable}}GITLAB_TOKEN{{/environmentVariable}}", (*(*services)["gitlab"].GetOptions())["AUTHENTICATION_TOKEN"])
+
+	substitutions, err := simpleConfigurationLayer.GetSubstitutions()
+	assert.NoError(t, err)
+	assert.NotNil(t, substitutions)
+
+	assert.Equal(t, 0, len(*substitutions.GetEnabled()))
+	assert.Equal(t, 8, len(*substitutions.GetItems()))
+}
+
+func TestPresetsLoadExtendedPresetFromGlobalConfiguration(t *testing.T) {
+	configuration, _ := NewConfiguration()
+	simpleConfigurationLayer := NewSimpleConfigurationLayer()
+	simpleConfigurationLayer.SetPreset(utl.PointerToString(EXTENDED_NAME))
+	var scl ConfigurationLayer = simpleConfigurationLayer
+	configuration.WithPluginConfiguration(&scl)
+
+	changelog, err := configuration.GetChangelog()
+	assert.NoError(t, err)
+	assert.NotNil(t, changelog)
+
+	assert.Equal(t, 4, len(*changelog.GetSections()))
+	assert.Equal(t, 0, len(*changelog.GetSubstitutions()))
+
+	commitMessageConventions, err := configuration.GetCommitMessageConventions()
+	assert.NoError(t, err)
+	assert.NotNil(t, commitMessageConventions)
+
+	assert.Equal(t, 2, len(*commitMessageConventions.GetEnabled()))
+	assert.Equal(t, 2, len(*commitMessageConventions.GetItems()))
+
+	releaseTypes, err := configuration.GetReleaseTypes()
+	assert.NoError(t, err)
+	assert.NotNil(t, releaseTypes)
+
+	assert.Equal(t, 9, len(*releaseTypes.GetEnabled()))
+	assert.Equal(t, 9, len(*releaseTypes.GetItems()))
+
+	services, err := configuration.GetServices()
+	assert.NoError(t, err)
+	assert.NotNil(t, services)
+
+	assert.Equal(t, 2, len(*services))
+	assert.Equal(t, ent.GITHUB, *(*services)["github"].GetType())
+	assert.Equal(t, 1, len(*(*services)["github"].GetOptions()))
+	assert.Equal(t, "{{#environmentVariable}}GITHUB_TOKEN{{/environmentVariable}}", (*(*services)["github"].GetOptions())["AUTHENTICATION_TOKEN"])
+	assert.Equal(t, ent.GITLAB, *(*services)["gitlab"].GetType())
+	assert.Equal(t, 1, len(*(*services)["gitlab"].GetOptions()))
+	assert.Equal(t, "{{#environmentVariable}}GITLAB_TOKEN{{/environmentVariable}}", (*(*services)["gitlab"].GetOptions())["AUTHENTICATION_TOKEN"])
+
+	substitutions, err := configuration.GetSubstitutions()
+	assert.NoError(t, err)
+	assert.NotNil(t, substitutions)
+
+	assert.Equal(t, 0, len(*substitutions.GetEnabled()))
+	assert.Equal(t, 0, len(*substitutions.GetItems()))
 }
--- src/java/main/src/test/java/com/mooltiverse/oss/nyx/configuration/presets/PresetsTests.java
+++ src/java/main/src/test/java/com/mooltiverse/oss/nyx/configuration/presets/PresetsTests.java
@@ -17,7 +17,11 @@ package com.mooltiverse.oss.nyx.configuration.presets;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.mooltiverse.oss.nyx.configuration.Configuration;
+import com.mooltiverse.oss.nyx.configuration.ConfigurationLayer;
+import com.mooltiverse.oss.nyx.configuration.SimpleConfigurationLayer;
 import com.mooltiverse.oss.nyx.entities.IllegalPropertyException;
+import com.mooltiverse.oss.nyx.entities.Provider;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -33,5 +37,112 @@ public class PresetsTests {
         assertThrows(IllegalPropertyException.class, () -> Presets.byName("notapreset"));
 
         assertNotNull(Presets.byName(Simple.NAME));
+        assertNotNull(Presets.byName(Extended.NAME));
+    }
+
+    @Test
+    @DisplayName("Load Simple Preset directly")
+    void loadSimplePresetDirectlyTest()
+        throws Exception {
+        ConfigurationLayer configurationLayer = Presets.byName(Simple.NAME);
+
+        assertNotNull(configurationLayer);
+
+        assertEquals(0, configurationLayer.getChangelog().getSections().size());
+        assertEquals(0, configurationLayer.getChangelog().getSubstitutions().size());
+
+        assertEquals(1, configurationLayer.getCommitMessageConventions().getEnabled().size());
+        assertEquals(1, configurationLayer.getCommitMessageConventions().getItems().size());
+
+        assertEquals(2, configurationLayer.getReleaseTypes().getEnabled().size());
+        assertEquals(2, configurationLayer.getReleaseTypes().getItems().size());
+
+        assertEquals(0, configurationLayer.getServices().size());
+
+        assertEquals(0, configurationLayer.getSubstitutions().getEnabled().size());
+        assertEquals(0, configurationLayer.getSubstitutions().getItems().size());
+    }
+
+    @Test
+    @DisplayName("Load Simple Preset from global configuration")
+    void loadSimplePresetFromGlobalConfigurationTest()
+        throws Exception {
+        Configuration configuration = new Configuration();
+        SimpleConfigurationLayer simpleConfigurationLayer = new SimpleConfigurationLayer();
+        simpleConfigurationLayer.setPreset(Simple.NAME);
+        configuration.withPluginConfiguration(simpleConfigurationLayer);
+
+        assertEquals(0, configuration.getChangelog().getSections().size());
+        assertEquals(0, configuration.getChangelog().getSubstitutions().size());
+
+        assertEquals(1, configuration.getCommitMessageConventions().getEnabled().size());
+        assertEquals(1, configuration.getCommitMessageConventions().getItems().size());
+
+        assertEquals(2, configuration.getReleaseTypes().getEnabled().size());
+        assertEquals(2, configuration.getReleaseTypes().getItems().size());
+
+        assertEquals(0, configuration.getServices().size());
+
+        assertEquals(0, configuration.getSubstitutions().getEnabled().size());
+        assertEquals(0, configuration.getSubstitutions().getItems().size());
+    }
+
+    @Test
+    @DisplayName("Load Extended Preset directly")
+    void loadExtendedPresetDirectlyTest()
+        throws Exception {
+        ConfigurationLayer configurationLayer = Presets.byName(Extended.NAME);
+
+        assertNotNull(configurationLayer);
+
+        assertEquals(4, configurationLayer.getChangelog().getSections().size());
+        assertEquals(0, configurationLayer.getChangelog().getSubstitutions().size());
+
+        assertEquals(2, configurationLayer.getCommitMessageConventions().getEnabled().size());
+        assertEquals(3, configurationLayer.getCommitMessageConventions().getItems().size());
+
+        assertEquals(9, configurationLayer.getReleaseTypes().getEnabled().size());
+        assertEquals(9, configurationLayer.getReleaseTypes().getItems().size());
+
+        assertEquals(2, configurationLayer.getServices().size());
+        assertEquals(Provider.GITHUB, configurationLayer.getServices().get("github").getType());
+        assertEquals(1, configurationLayer.getServices().get("github").getOptions().size());
+        assertEquals("{{#environmentVariable}}GITHUB_TOKEN{{/environmentVariable}}", configurationLayer.getServices().get("github").getOptions().get("AUTHENTICATION_TOKEN"));
+        assertEquals(Provider.GITLAB, configurationLayer.getServices().get("gitlab").getType());
+        assertEquals(1, configurationLayer.getServices().get("gitlab").getOptions().size());
+        assertEquals("{{#environmentVariable}}GITLAB_TOKEN{{/environmentVariable}}", configurationLayer.getServices().get("gitlab").getOptions().get("AUTHENTICATION_TOKEN"));
+
+        assertEquals(0, configurationLayer.getSubstitutions().getEnabled().size());
+        assertEquals(8, configurationLayer.getSubstitutions().getItems().size());
+    }
+
+    @Test
+    @DisplayName("Load Extended Preset from global configuration")
+    void loadExtendedPresetFromGlobalConfigurationTest()
+        throws Exception {
+        Configuration configuration = new Configuration();
+        SimpleConfigurationLayer simpleConfigurationLayer = new SimpleConfigurationLayer();
+        simpleConfigurationLayer.setPreset(Extended.NAME);
+        configuration.withPluginConfiguration(simpleConfigurationLayer);
+
+        assertEquals(4, configuration.getChangelog().getSections().size());
+        assertEquals(0, configuration.getChangelog().getSubstitutions().size());
+
+        assertEquals(2, configuration.getCommitMessageConventions().getEnabled().size());
+        assertEquals(2, configuration.getCommitMessageConventions().getItems().size());
+
+        assertEquals(9, configuration.getReleaseTypes().getEnabled().size());
+        assertEquals(9, configuration.getReleaseTypes().getItems().size());
+
+        assertEquals(2, configuration.getServices().size());
+        assertEquals(Provider.GITHUB, configuration.getServices().get("github").getType());
+        assertEquals(1, configuration.getServices().get("github").getOptions().size());
+        assertEquals("{{#environmentVariable}}GITHUB_TOKEN{{/environmentVariable}}", configuration.getServices().get("github").getOptions().get("AUTHENTICATION_TOKEN"));
+        assertEquals(Provider.GITLAB, configuration.getServices().get("gitlab").getType());
+        assertEquals(1, configuration.getServices().get("gitlab").getOptions().size());
+        assertEquals("{{#environmentVariable}}GITLAB_TOKEN{{/environmentVariable}}", configuration.getServices().get("gitlab").getOptions().get("AUTHENTICATION_TOKEN"));
+
+        assertEquals(0, configuration.getSubstitutions().getEnabled().size());
+        assertEquals(0, configuration.getSubstitutions().getItems().size());
     }
 }
